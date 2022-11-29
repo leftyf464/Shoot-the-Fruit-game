e894e76ce0965493457a93dcc5fc8ace1ad9f646
leftyf464 <77945154+leftyf464@users.noreply.github.com>
2022-11-28 21:52:08 -0800

    Add files via upload

---
+++ shoot.py
@@ -0,0 +1,32 @@
+
+
+from random import randint
+
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+
+     apple.x = randint(10, 800)
+     apple.y = randint(10, 600)
+
+
+def on_mouse_down(pos):
+
+    if apple.collidepoint(pos):
+        
+        print("Good Shooting!")
+        place_apple()
+
+    else:
+
+        print("Missed it!")
+        quit()
+
+
+
+place_apple()
+ 
